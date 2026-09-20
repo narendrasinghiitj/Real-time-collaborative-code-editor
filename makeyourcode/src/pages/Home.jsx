f34bad3ae6b8321c7bbb78f16a457b8bc678259c
[narendra singh] <b25ec1027@iitj.ac.in>
2026-09-21 02:13:40 +0530

    Home page style and uniqueid

--- makeyourcode/src/pages/Home.jsx
+++ makeyourcode/src/pages/Home.jsx
@@ -1,26 +1,39 @@
 import React, {useState} from "react";
+import {v4 as uuidV4} from 'uuid';
 
 const Home = () => {
+    const [roomId,setRoomId] = useState('');
+    const [username,setUsername] = useState('');
+    const createNewRoom = (e) =>{
+        e.preventDefault();
+        const id = uuidV4();
+        setRoomId(id);
+        
+    }
     return (
     <div className="homepageWrapper">
         <div className="formWrapper">
-            <img src="/code-sync.png" alt="code-sync-logo"/>
+            <img className="" src="homePageLogo" alt="code-sync-logo"/>
             <h4 className="mainLabel">Paste Invitation ROOM ID</h4>
             <div className="inputGroup">
                 <input 
                     type="text"
                     className="inputBox"
                     placeholder="ROOM ID"
+                    onChange = {(e)=> setRoomId(e.target.value)}
+                    value={roomId}
                 />
                 <input 
                     type="text"
                     className="inputBox"
                     placeholder="USERNAME"
+                    onChange = {(e)=> setUsername(e.target.value)}
+                    value={username}
                 />
-                <button className="btn joinBtn"></button>
+                <button className="btn joinBtn">Join</button>
                 <span className="createInfo">
                     If you don't have an invite then create &nbsp;
-                    <a href="" className="createNewBtn">
+                    <a onClick={createNewRoom} href="" className="createNewBtn">
                         new room
                     </a>
                 </span>
