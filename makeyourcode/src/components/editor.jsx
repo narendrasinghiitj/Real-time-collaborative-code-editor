f03bcb4666e4f92b9940759d4879bf0f2cfd5f81
[narendra singh] <b25ec1027@iitj.ac.in>
2026-09-25 01:42:41 +0530

    Editor page design and style, textArea design

---
+++ makeyourcode/src/components/editor.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+import Codemirror from 'codemirror'
+
+const editor = () => {
+    useEffect(() => {
+        async function init(){
+
+        }
+        init();
+    }, []);
+    return <textarea id="realtimeEditor"></textarea>
+}
+
+export default editor
--- makeyourcode/src/pages/EditorPage.jsx
+++ makeyourcode/src/pages/EditorPage.jsx
@@ -1,7 +1,34 @@
 import React, {useState} from "react";
+import Client from '../components/Clinet';
+import Editor from '../components/editor';
 
 const EditorPage = () =>{
-    return <div>Editor Page</div>;
+    const [clients, setClients] = useState([
+        {socketId: 1, username: 'narendra'},
+        {socketId: 2, username: 'Arjun'},
+        {socketId: 3, username: 'Payal'}
+    ]);
+    return <div className="mainWrap">
+        <div className="aside">
+            <div className="asideInner">
+                <div className="logo">
+                    <img src="" alt="logo" className="logoImage" />
+                </div>
+                <h3 className="">Connected</h3>
+                <div className="clientList">
+                    {clients.map((client) => (
+                        <Client key={client.socketId } username={client.username}/>
+                    ))}
+                </div>
+            </div>
+            <button className="btn copyBtn">Copy Room Id</button>
+            <button className="btn leaveBtn">LEAVE</button>
+
+        </div>
+        <div className="editorWrap">
+            <Editor />
+        </div>
+    </div>;
 };
 
 export default EditorPage;
